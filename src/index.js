ed3aa6056393463e6e09fdfbac333cb933608f06
tegarpenemuan <tegar@aksaramaya.com>
2023-03-13 09:30:03 +0700

    MVC dengan module syntax

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-// import { Express } from "express"; //ES6 module Syntak
-// "type": "module" // di packeage.json atas script
-const Express = require("express"); // import express
-const Router = require("./routes/router"); // import router
-// init express
-const app = Express();
-app.use(Router);
+//import express
+import express from "express";
 
-//listen on port
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
+// import router
+import router from "./routes/router.js";
+
+// init express
+const app = express();
+
+// Use Router
+app.use(router);
+
+// listen on port
+app.listen(3000, () => console.log("Server Running at http://localhost:3000"));
